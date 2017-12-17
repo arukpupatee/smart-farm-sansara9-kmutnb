2898f65eafb6619dc9e698d11d8ca799596773b4
Tanawat Horsirimanon <galiblenight@gmail.com>
2017-12-17 10:46:46 +0630

    test

--- test.js
+++ test.js
@@ -26,7 +26,7 @@ for(let i = 1; i < 5; i++){
 var name_val = ['air_temperature', 'air_humidity', 'brightness', 'soil_temperature', 'soil_moisture']
 
 for(let n in name_val){
-  let num = Math.random()
+  let num = parseInt(Math.random()*1000)
   console.log(num);
   request('http://127.0.0.1:5000/api/insert/'+name_val[n]+'/1/1/'+num, (error, response, body1) => {
     request('http://127.0.0.1:5000/api/get/'+name_val[n]+'/1/1', (error, response, body2) => {
