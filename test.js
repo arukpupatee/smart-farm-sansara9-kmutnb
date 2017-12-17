f964cd29492c60680e66c9efa271625e666ab5f9
Tanawat Horsirimanon <galiblenight@gmail.com>
2017-12-17 09:45:33 +0630

    test

--- test.js
+++ test.js
@@ -20,3 +20,16 @@ for(let i = 1; i < 5; i++){
     })
   })
 }
+
+// console.log();
+
+var name_val = ['air_temperature', 'air_humidity', 'brightness', 'soil_temperature', 'soil_moisture']
+
+for(var n in name_val){
+  let num = parseInt(Math.random()*1000)
+  request('http://127.0.0.1:5000/api/insert/'+n+'/1/1/'+num, (error, response, body1) => {
+    request('http://127.0.0.1:5000/api/get/'+n+'/1/1', (error, response, body2) => {
+      assert((body1 == 'ok') && (body2 == num), 'Set "OFF" value on '+n+' as '+num+' but value is not '+num)
+    })
+  })
+}
